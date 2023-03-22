fd6f913aed466327ae532d49b7d1167d1e600a51
bharathvenkateswaran <bharathvenkateswaran03@gmail.com>
2023-03-22 10:47:37 -0500

    updated

--- Project1SimpleFileModifier.cpp
+++ Project1SimpleFileModifier.cpp
@@ -1,53 +1,19 @@
+// Import necessary headers
 #include "Project1SimpleFileModifier.h"
 #include "FileModifyException.h"
 #include "Util.h"
-#include <fstream>
+
 #include <iostream>
+#include <fstream>
 #include <cstring>
 
+// Define function to modify and copy a file
 void Project1SimpleFileModifier::modifyAndCopyFile(const char* sourceFile, const char* destFile) {
-    // Open input file
-    std::ifstream input(Util::inputFilename, std::ios::binary);
-    if (!input.is_open()) {
-        throw FileModifyException("Failed to open input file");
+    FileCopier::makeCopy(sourceFile, destFile):
+
+
     }
 
-    // Open output file
-    std::ofstream output(Util::outputFilename, std::ios::binary);
-    if (!output.is_open()) {
-        throw FileModifyException("Failed to open output file");
-    }
 
-    // Read the number of entries
-    int numEntries;
-    input.read(reinterpret_cast<char*>(&numEntries), sizeof(numEntries));
 
-    // Modify the first entry
-    EntryInfo firstEntry;
-    input.read(reinterpret_cast<char*>(&firstEntry), sizeof(firstEntry));
-    firstEntry.price = 1000000.0;
-    firstEntry.weight = 2.25;
-    strncpy(firstEntry.itemName, "CS 3377", sizeof(firstEntry.itemName));
-    output.write(reinterpret_cast<const char*>(&firstEntry), sizeof(firstEntry));
 
-    // Write the rest of the entries
-    for (int i = 1; i < numEntries; i++) {
-        EntryInfo entry;
-        input.read(reinterpret_cast<char*>(&entry), sizeof(entry));
-        output.write(reinterpret_cast<const char*>(&entry), sizeof(entry));
-    }
-
-    // Add a new entry
-    EntryInfo newEntry;
-    newEntry.itemID = 6530927;
-    strncpy(newEntry.itemName, "Advanced Programming in the UNIX Environment by Stevens and Rago", sizeof(newEntry.itemName));
-    newEntry.quantity = 77;
-    newEntry.price = 89.99;
-    newEntry.weight = 3.0;
-    output.write(reinterpret_cast<const char*>(&newEntry), sizeof(newEntry));
-    numEntries++;
-
-    // Update the number of entries
-    output.seekp(0, std::ios::beg);
-    output.write(reinterpret_cast<const char*>(&numEntries), sizeof(numEntries));
-}
