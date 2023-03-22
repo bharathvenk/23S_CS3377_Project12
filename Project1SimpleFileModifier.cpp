2a2677c27886da36944b83e6793c920a69e4b16b
bharathvenkateswaran <bharathvenkateswaran03@gmail.com>
2023-03-22 12:00:27 -0500

    updated

--- Project1SimpleFileModifier.cpp
+++ Project1SimpleFileModifier.cpp
@@ -7,12 +7,40 @@
 #include <fstream>
 #include <cstring>
 
+
+
+
+#include <fcntl.h>
+#include <unistd.h>
+
+
 // Define function to modify and copy a file
 void Project1SimpleFileModifier::modifyAndCopyFile(const char* sourceFile, const char* destFile) {
-    FileCopier::makeCopy(sourceFile, destFile):
-
-
+    // Open
+    int sourceFd = open(sourceFile, O_RDONLY);
+    if (sourceFd == -1) {
+        throw FileModifyException("  Error opening source file.");
     }
+
+
+    std::ofstream destFileStream(destFile);
+    if (!destFileStream) {
+        close(sourceFd);
+        throw FileModifyException("Error creating destination file.");
+    }
+
+
+    char buffer[BUFSIZ];
+    ssize_t bytesRead;
+    while ((bytesRead = read(sourceFd, buffer, BUFSIZ)) > 0) {
+        destFileStream.write(buffer, bytesRead);
+    }
+
+
+    close(sourceFd);
+    destFileStream.close();
+}
+
 
 
 
